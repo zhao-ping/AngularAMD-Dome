3f505018b32a928c958f6ad69ed06bb48e7ba17c
zhao-ping <2362331574@qq.com>
2016-09-21 14:34:46 +0800

    Up code

---
+++ app/app.js
@@ -0,0 +1,37 @@
+/*
+                   _ooOoo_
+                  o8888888o
+                  88" . "88
+                  (| -_- |)
+                  O\  =  /O
+               ____/`---'\____
+             .'  \\|     |//  `.
+            /  \\|||  :  |||//  \
+           /  _||||| -:- |||||-  \
+           |   | \\\  -  /// |   |
+           | \_|  ''\---/''  |   |
+           \  .-\__  `-`  ___/-. /
+         ___`. .'  /--.--\  `. . __
+      ."" '<  `.___\_<|>_/___.'  >'"".
+     | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+     \  \ `-.   \_ __\ /__ _/   .-` /  /
+======`-.____`-.___\_____/___.-`____.-'======
+                   `=---='
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+         佛祖保佑       永无BUG ，Feel
+*/
+
+define(["angularAMD","angular-route"], function (angularAMD) {
+    var app = angular.module("app", ["ngRoute"]);
+    
+    //APP路由配置
+    app.config(["$routeProvider",function (a) {
+        a.when("/home", angularAMD.route({
+            templateUrl: 'views/index.html',
+            //controller: 'indexCtrl',
+            //controllerUrl: 'controller/index'
+        }))
+        .otherwise({redirectTo:"/home"})
+    }]);
+    return angularAMD.bootstrap(app);
+});
---
+++ app/main.js
@@ -0,0 +1,27 @@
+//当前版本号
+var version = "1.0";
+//require配置
+require.config({
+    baseUrl: "app/",
+    map: {
+        '*': {
+           'css': 'css/css.min'
+        }
+    },
+	// alias libraries paths
+    paths: {
+        'angular': 'js/angular.min',
+        "angular-route": "js/angular-route",
+        'angularAMD': 'js/angularAMD.min',
+    },
+    // Add angular modules that does not support AMD out of the box, put it in a shim
+    shim: {
+         'angularAMD' : ['angular'],
+         'angular-route' : ['angular']
+    },
+
+    // kick start application
+    deps: ['app'],
+    urlArgs: version
+});
+
